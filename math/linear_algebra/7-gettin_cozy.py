3b3f6666a7e1cffe6cf51aac728477ce566f863e
leila7575 <leila.louajri@gmail.com>
2024-12-06 21:36:29 +0100

    updating 7-gettin_cozy file

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,8 +6,15 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """Concatenates two matrices."""
+    new_matrix = []
     if axis == 1:
-        new_matrix = [mat1[i] + mat2[i] for i in range(len(mat1))]
+        if len(mat1) == len(mat2):
+            new_matrix = [mat1[i] + mat2[i] for i in range(len(mat1))]
+            return new_matrix
+        return None
     if axis == 0:
-        new_matrix = mat1 + mat2
-    return new_matrix
+        if len(mat1[0]) == len(mat2[0]):
+            new_matrix = mat1 + mat2
+            return new_matrix
+        return None
+    return None
