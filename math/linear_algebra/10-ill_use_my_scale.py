3077d9576908b27ff27d1031ed4cedd009daa5c5
leila7575 <leila.louajri@gmail.com>
2024-12-06 11:42:17 +0100

    adding shape function file

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+ This module contains the function mat_mul.
+"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """Calculates the shape of an array"""
+    return matrix.shape
