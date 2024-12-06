94158073b21cd6678c5b32e259abbe50806ea066
leila7575 <leila.louajri@gmail.com>
2024-12-06 19:34:04 +0100

    10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -2,9 +2,8 @@
 """
  This module contains the function mat_mul.
 """
-import numpy as np
 
 
 def np_shape(matrix):
-    """Calculates the shape of an array"""
-    return matrix.shape
+    """Calculates the shape of a numpy.ndarray"""
+    return tuple(matrix.shape)
