b78a659fddfa17309b242d43eaf356a0cba77e0b
leila7575 <leila.louajri@gmail.com>
2024-12-06 21:10:53 +0100

    adding 14-saddle_up file

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+ This module contains the function np_matmul.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Multiply matrices"""
+    return np.matmul(mat1, mat2)
