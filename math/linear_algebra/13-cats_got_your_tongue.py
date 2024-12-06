f23bf76bc95d67c2c7571e427c034f8f8b68cb02
leila7575 <leila.louajri@gmail.com>
2024-12-06 21:05:32 +0100

    adding 13-cats_got_your_tongue file

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -7,8 +7,4 @@ import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """Concatenates matrices"""
-    if axis == 1:
-        concatenated_array = np.column_stack((mat1, mat2))
-    if axis == 0:
-        concatenated_array = np.concatenate((mat1, mat2))
-    return concatenated_array
+    return np.concatenate((mat1, mat2), axis)
