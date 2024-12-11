64dafa5ec5104e35910227fd4e3639a3351d2c85
leila7575 <leila.louajri@gmail.com>
2024-12-11 13:03:51 +0100

    updating 0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -5,9 +5,10 @@ import matplotlib.pyplot as plt
 
 def line():
 
-    y = np.arange(0, 10) ** 3
+    y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 10)
-    plt.plot(x, y, linestyle='solid', color='red')
+    plt.plot(range(11), y, linestyle='solid', color='red')
+    plt.xlim(0, 10)
     plt.show()
