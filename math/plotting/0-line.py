7fa302a84a93647bc3696bbb395d41047a3fd26b
leila7575 <leila.louajri@gmail.com>
2024-12-11 13:09:13 +0100

    updating 0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
+"""
+ This module contains the function line.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """Defines a graph with y = x ^ 3 and x in range 0 to 10"""
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
