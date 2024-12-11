746b889825f65e6877a5f5f18782f039dc959a99
leila7575 <leila.louajri@gmail.com>
2024-12-11 12:44:51 +0100

    updating 0-line

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -2,11 +2,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def line():
 
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     x = np.arange(0, 11)
-    plt.plot(x, y)
+    plt.plot(x, y, color='red')
     plt.show()
