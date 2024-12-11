8e80cf01ff3a85dbb2081f0aff4b9a44177bcb43
leila7575 <leila.louajri@gmail.com>
2024-12-11 13:14:43 +0100

    updating 1-scatter.py

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
+"""
+ This module contains the function scatter.
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
+    """Defines a scatter plot with x axis representing height values
+    weight values represented on y-axis"""
 
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
@@ -13,6 +19,5 @@ def scatter():
     plt.title("Men's Height vs Weight")
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
-    plt.scatter(x, y, color = 'magenta')
+    plt.scatter(x, y, color='magenta')
     plt.show()
-    
