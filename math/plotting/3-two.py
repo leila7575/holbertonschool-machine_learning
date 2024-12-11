9228e2598ba62bb568e7949512de770fdae0af63
leila7575 <leila.louajri@gmail.com>
2024-12-11 21:31:07 +0100

    adding exponential decay of radioactive elements graph file

---
+++ math/plotting/3-two.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+"""
+ This module contains the function two.
+"""
+
+
+def two():
+    """Defines a line graph representing C14 and Ra-226 exponential decay"""
+    x = np.arange(0, 21000, 1000)
+    r = np.log(0.5)
+    t1 = 5730
+    t2 = 1600
+    y1 = np.exp((r / t1) * x)
+    y2 = np.exp((r / t2) * x)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlim(0, 20000)
+    plt.ylim(0, 1)
+    plt.title("Exponential Decay of Radioactive Elements")
+    plt.xlabel("Time (years)")
+    plt.ylabel("Fraction Remaining")
+    plt.plot(x, y1, linestyle='--', color='red')
+    plt.plot(x, y2, linestyle='-', color='green')
+    plt.legend(['C-14', 'Ra-226'], loc='upper right')
+    plt.show()
