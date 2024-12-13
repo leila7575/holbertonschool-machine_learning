04bdd03eb240b16cb6f32e59f47a2a451a2799e2
leila7575 <leila.louajri@gmail.com>
2024-12-13 23:35:27 +0100

    17-integrate.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+ This module contains the function poly_integral.
+"""
+
+
+def poly_integral(poly, C=0):
+    """Calculates the integral of a polynomial."""
+    if not isinstance(poly, list) or not isinstance(C, int):
+        return None
+    integral = [C]
+    for exp, coefficient in enumerate(poly):
+        integral_coefficient = coefficient / (exp + 1)
+        if integral_coefficient.is_integer():
+            integral.append(int(integral_coefficient))
+        else:
+            integral.append(integral_coefficient)
+    return integral
