cf0990d6a5d476ac4eba532cfd7fcfed8a152348
leila7575 <leila.louajri@gmail.com>
2024-12-13 10:20:31 +0100

    10-matisse.py

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+ This module contains the function poly_derivative.
+"""
+
+
+def poly_derivative(poly):
+    """Calculates the derivative of a polynomial."""
+    derivative = []
+    if not isinstance(poly, list):
+        return None
+    for i in range(1, len(poly)):
+        derivative.append(poly[i] * i)
+    return derivative
