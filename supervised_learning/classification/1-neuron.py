1d12680073180c503a309457e4270f73443420c1
leila7575 <leila.louajri@gmail.com>
2024-12-31 00:53:11 +0100

    updating neuron class file with private attributes

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""This module defines a single neuron for binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron for binary classification."""
+    def __init__(self, nx):
+        """Class constructor. Initializes the neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be positive")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Retrieves the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Retrieves the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Retrieves the activated output."""
+        return self.__A
