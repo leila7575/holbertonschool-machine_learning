d94454040f1a5ff615b566b2fd74d7c7413606d5
leila7575 <leila.louajri@gmail.com>
2025-01-07 18:47:29 +0100

    5-neuron.py

---
+++ supervised_learning/classification/5-neuron.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""This module defines a single neuron for binary classification."""
+
+import numpy as np
+
+
+class Neuron:
+    """Defines a single neuron for binary classification."""
+    def __init__(self, nx):
+        """Class constructor. Initializes the neuron."""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be positive")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Retrieves the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Retrieves the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Retrieves the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates forward propagation of the neuron."""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1/(1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """Calculates cost of the model based on logistic regression"""
+        m = Y.shape[1]
+        cost = -1/m * np.sum(
+            np.multiply(np.log(A), Y) + np.multiply(
+                np.log(1.0000001 - A), 1 - Y
+                )
+            )
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neuron's prediction.
+        Returns the prediction and the cost."""
+        A = self.forward_prop(X)
+        prediction = (A >= 0.5).astype(int)
+        cost = self.cost(Y, A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """Calculates one pass of gradient descent."""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = (1/m) * np.dot(dZ, X.T)
+        db = (1/m) * np.sum(dZ)
+        self.__W = self.__W - (alpha * dW)
+        self.__b = self.__b - (alpha * db)
