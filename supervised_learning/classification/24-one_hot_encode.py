4adb8e488b9b7c9d709f29838565eab32d85d52e
leila7575 <leila.louajri@gmail.com>
2025-01-08 18:45:32 +0100

    24-one_hot_encode.py

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -8,6 +8,11 @@ def one_hot_encode(Y, classes):
     """Converts a numeric label vector into a one-hot matrix."""
     m = Y.shape[0]
     matrix = np.zeros((classes, m))
+    
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+        return None
+
     for i in range(m):
         matrix[Y[i], i] = 1
+
     return matrix
