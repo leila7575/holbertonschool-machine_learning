57f0d06f59453746c47e1e71bcd80563dc494db5
leila7575 <leila.louajri@gmail.com>
2025-01-08 18:35:12 +0100

    24-one_hot_encode.py

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module defines the one_hot_encode function"""
+
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """Converts a numeric label vector into a one-hot matrix."""
+    m = Y.shape[0]
+    matrix = np.zeros((classes, m))
+    for i in range(m):
+        matrix[Y[i], i] = 1
+    return matrix
