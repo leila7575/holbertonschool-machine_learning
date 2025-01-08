4549232fda8ce9410513d6e444c3282448901b5e
leila7575 <leila.louajri@gmail.com>
2025-01-08 19:52:09 +0100

    25-one_hot_decode.py

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module defines the one_hot_decode function"""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """Converts a one-hot matrix into a vector of labels."""
+
+    label_vector = []
+
+    for i in one_hot.T:
+        for index, value in enumerate(i):
+            if value == 1:
+                label_vector.append(index)
+
+    return np.array(label_vector)
