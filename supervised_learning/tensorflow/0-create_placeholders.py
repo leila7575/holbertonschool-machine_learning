281c00e1cc68fbb3605de8e8a8ff816128c7790c
leila7575 <leila.louajri@gmail.com>
2025-01-10 14:25:35 +0100

    adding 0-create_placeholder

---
+++ supervised_learning/tensorflow/0-create_placeholders.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module contains the function create_placeholder."""
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+
+def create_placeholders(nx, classes):
+    """Defines two placeholders x and y"""
+    x = tf.placeholder(tf.float32, [None, nx], name="x")
+    y = tf.placeholder(tf.float32, [None, classes], name="y")
+    return x, y
