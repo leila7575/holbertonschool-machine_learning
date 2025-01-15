38c80682e6d027d718a166793b396ec510de8fb5
leila7575 <leila.louajri@gmail.com>
2025-01-15 15:46:38 +0100

    fixing pycodestyle

--- supervised_learning/tensorflow/4-calculate_loss.py
+++ supervised_learning/tensorflow/4-calculate_loss.py
@@ -9,8 +9,8 @@ tf.disable_v2_behavior()
 def calculate_loss(y, y_pred):
     """Calculates the softmax cross entropy loss of a prediction."""
     with tf.name_scope('softmax_cross_entropy_loss'):
-        cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_pred)
-        average_loss = tf.reduce_mean(
-            cross_entropy_loss, name='value'
+        cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(
+            labels=y, logits=y_pred
             )
+        average_loss = tf.reduce_mean(cross_entropy_loss, name='value')
     return average_loss
