92c4e6a51d17929a92716caea67fdbe4e7fdce33
leila7575 <leila.louajri@gmail.com>
2025-01-17 16:40:32 +0100

    updating 0-sequential file import

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -3,25 +3,23 @@
 
 
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-from tensorflow.keras import layers, regularizers
+import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Builds a neural network."""
-    model = Sequential()
+    model = K.models.Sequential()
 
-    model.add(Dense(
+    model.add(K.layers.Dense(
         units=layers[0], activation=activations[0],
-        kernel_regularizer=regularizers.L2(l2=lambtha), input_shape=(nx, )
+        kernel_regularizer=K.regularizers.L2(l2=lambtha), input_shape=(nx, )
         ))
-    model.add(Dropout(rate=1 - keep_prob))
+    model.add(K.layers.Dropout(rate=1 - keep_prob))
 
     for units, activation in zip(layers[1:], activations[1:]):
-        model.add(Dense(
+        model.add(K.layers.Dense(
             units=units, activation=activation,
-            kernel_regularizer=regularizers.L2(l2=lambtha)
+            kernel_regularizer=K.regularizers.L2(l2=lambtha)
             ))
-        model.add(Dropout(rate=1 - keep_prob))
+        model.add(K.layers.Dropout(rate=1 - keep_prob))
     return model
