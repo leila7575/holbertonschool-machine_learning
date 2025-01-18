550a12a02d688cfedfc27d97520bd568635019db
leila7575 <leila.louajri@gmail.com>
2025-01-18 18:14:45 +0100

    3-one_hot.py

---
+++ supervised_learning/keras/3-one_hot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""This module contains the function optimize_model."""
+
+
+import tensorflow as tf
+import tensorflow.keras as K
+
+
+def one_hot(labels, classes=None):
+    """Converts a label vector into a one-hot matrix"""
+    if classes is None:
+        classes = max(labels) + 1
+    one_hot_matrix = K.utils.to_categorical(labels, classes)
+    return one_hot_matrix
