641c75e3186086651e7909a5c110053f84bddad4
leila7575 <leila.louajri@gmail.com>
2025-01-19 20:46:46 +0100

    12-test.py

---
+++ supervised_learning/keras/12-test.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains the function test_model."""
+
+
+import tensorflow.keras as K
+
+
+def test_model(network, data, labels, verbose=True):
+    """Tests a model"""
+    test_loss, test_accuracy = network.evaluate(data, labels, verbose)
+    return test_loss, test_accuracy
