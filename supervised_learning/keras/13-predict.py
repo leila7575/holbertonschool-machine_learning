40f081e8d3d3b77fb907ce0c0d34ddde64f88466
leila7575 <leila.louajri@gmail.com>
2025-01-19 20:54:38 +0100

    13-predict.py

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This module contains the function predict."""
+
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """Tests a model"""
+    predictions = network.predict(data, labels, verbose=verbose)
+    return [predictions]
