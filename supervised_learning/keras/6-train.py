57ca9fb887ba5e16b73e6068a1db96318e37009e
leila7575 <leila.louajri@gmail.com>
2025-01-19 17:35:13 +0100

    6-train.py

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -10,18 +10,20 @@ def train_model(
     early_stopping=False, patience=0, verbose=True, shuffle=False
 ):
     """Trains the model with mini-batch gradient descent."""
+    callbacks = []
     if validation_data is not None:
         early_stopping = K.callbacks.EarlyStopping(
             monitor='val_loss', patience=patience
         )
+        callbacks.append(early_stopping)
     history = network.fit(
         x=data,
         y=labels,
         batch_size=batch_size,
         epochs=epochs,
-        verbose=1 if verbose else 0,
+        verbose=verbose,
         validation_data=validation_data,
         shuffle=shuffle,
-        callbacks=[early_stopping]
-        )
+        callbacks=callbacks
+    )
     return history
