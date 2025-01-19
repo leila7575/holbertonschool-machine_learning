4581f7c63723e6bbe231a7a20e607c2cda413835
leila7575 <leila.louajri@gmail.com>
2025-01-19 20:15:26 +0100

    9-model.py

---
+++ supervised_learning/keras/9-model.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains the function save_model."""
+
+
+import tensorflow.keras as K
+
+
+def save_model(network, filename):
+    """saves the model."""
+    network.save(filename)
+    
+    
+def load_model(filename):
+    """loads the model."""
+    model = K.models.load_model(filename)
+    return model
