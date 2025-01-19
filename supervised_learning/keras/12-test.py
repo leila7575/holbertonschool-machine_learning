b3eaadd93bcab609d08dc7178e1c5b1ba69a4753
leila7575 <leila.louajri@gmail.com>
2025-01-19 20:49:29 +0100

    12-test.py

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -7,5 +7,5 @@ import tensorflow.keras as K
 
 def test_model(network, data, labels, verbose=True):
     """Tests a model"""
-    test_loss, test_accuracy = network.evaluate(data, labels, verbose)
-    return [test_loss, test_accuracy]
+    test_loss, test_accuracy = network.evaluate(data, labels, verbose=verbose)
+    return test_loss, test_accuracy
