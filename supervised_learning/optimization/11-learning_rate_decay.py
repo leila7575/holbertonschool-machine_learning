270e5ff1e707dd8c72c178e8f7136b83f32f73a1
leila7575 <leila.louajri@gmail.com>
2025-01-26 00:56:25 +0100

    11-learning_rate_decay.py

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module contains the function learning_rate_decay."""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """Updates learning rate with inverse time decay."""
+    step = round(global_step / decay_step)
+    updated_alpha = alpha / (1 + decay_rate * step)
+    return updated_alpha
