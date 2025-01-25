ef87faf58b93883f269b352d1d29c6ac7dae9d5b
leila7575 <leila.louajri@gmail.com>
2025-01-25 22:58:44 +0100

    adding 7-RMSProp file

--- supervised_learning/optimization/7-RMSProp.py
+++ supervised_learning/optimization/7-RMSProp.py
@@ -8,5 +8,5 @@ import numpy as np
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     """Updates a variable with RMSProp algorithm."""
     second_moment = beta2 * s + (1 - beta2) * (np.square(grad))
-    updated_variable = var - alpha * grad / (np.sqrt(second_moment + epsilon))
+    updated_variable = var - alpha * grad / (np.sqrt(second_moment) + epsilon)
     return updated_variable, second_moment
