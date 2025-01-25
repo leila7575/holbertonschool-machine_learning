88a99f729c96fd9af2325b9e69fa2c288751a6c6
leila7575 <leila.louajri@gmail.com>
2025-01-26 00:58:48 +0100

    11-learning_rate_decay.py

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -4,6 +4,6 @@
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """Updates learning rate with inverse time decay."""
-    step = round(global_step / decay_step)
+    step = global_step // decay_step
     updated_alpha = alpha / (1 + decay_rate * step)
     return updated_alpha
