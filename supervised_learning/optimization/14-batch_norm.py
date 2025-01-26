93fc4e940621023d154dc85827bc3e57e18901e2
leila7575 <leila.louajri@gmail.com>
2025-01-26 16:18:37 +0100

    14-batch_norm.py

---
+++ supervised_learning/optimization/14-batch_norm.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""This module contains the function create_batch_norm_layer."""
+
+
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """Creates a batch normalization layer for a neural network."""
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=tf.keras.initializers.VarianceScaling(mode='fan_avg'),
+        )(prev)
+    normalized_batch = tf.keras.layers.BatchNormalization(epsilon=1e-7)(layer)
+    output = activation(normalized_batch)
+    return output
