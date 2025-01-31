bfc62998275b78088c7a648a0f2307e3e9cbbd4d
leila7575 <leila.louajri@gmail.com>
2025-01-31 11:43:08 +0100

    0-l2_reg_cost.py

---
+++ supervised_learning/regularization/0-l2_reg_cost.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains the function l2_reg_cost."""
+
+
+import tensorflow as tf
+import numpy as np
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """Calculates the cost with L2 regularization."""
+    weight_squared_sum = 0
+    for i in range (1, L+1):
+        weight_squared_sum += np.linalg.norm(weights[f'W{i}'])**2
+    cost_l2 =(lambtha / (2 * m)) * weight_squared_sum
+    total_cost = cost + cost_l2
+    return total_cost
