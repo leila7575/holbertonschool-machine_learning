0f19012326ad84b8334bc3306f37ad56ee463184
leila7575 <leila.louajri@gmail.com>
2025-01-31 15:20:26 +0100

    2-l2_reg_cost.py

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,4 +6,6 @@ import tensorflow as tf
 
 
 def l2_reg_cost(cost, model):
+    """calculates cost by adding the cost without L2 regularization
+    and the total cost with L2 regularization."""
     return cost + model.losses
