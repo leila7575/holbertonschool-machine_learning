ac00bfcec116a68ebd75cab7c6e0db4878720492
leila7575 <leila.louajri@gmail.com>
2025-02-04 02:53:35 +0100

    1-convolve_grayscale_same.py

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -9,12 +9,16 @@ def convolve_grayscale_same(images, kernel):
     """Performs a convolution applying a kernel on a grayscale image."""
     m, h, w = images.shape
     kh, kw = kernel.shape
+    ph = (kh - 1)//2
+    pw = (kw - 1)//2
 
     res = np.zeros((m, h, w))
-    image_padded = np.zeros((m, h + 2, w + 2))
-    image_padded[:, 1:-1, 1:-1] = images
+    image_padded = np.zeros((m, h + 2 * ph, w + 2 * pw))
+    image_padded[:, ph:-ph, pw:-pw] = images
     for i in range(h):
         for j in range(w):
-            res[:, i, j] = np.sum(image_padded[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            res[:, i, j] = np.sum(
+                image_padded[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2)
+            )
 
     return res
