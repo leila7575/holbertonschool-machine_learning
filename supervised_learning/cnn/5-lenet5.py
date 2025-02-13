22b66d17f7222f4a6219e21d344f9f3e6b948589
leila7575 <leila.louajri@gmail.com>
2025-02-13 16:06:46 +0100

    5-lenet5.py

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -7,41 +7,40 @@ from tensorflow import keras as K
 
 def lenet5(X):
     """Builds a lenet5 convolutional network."""
-    model = K.models.Sequential([
-        K.layers.Conv2D(
-            6,
-            (5, 5),
-            activation='relu',
-            padding='same',
-            kernel_initializer=K.initializers.HeNormal(seed=None),
-            input_shape=(28, 28, 1)
-        ),
-        K.layers.MaxPooling2D((2, 2), strides=(2, 2)),
-        K.layers.Conv2D(
-            16,
-            (5, 5),
-            activation='relu',
-            padding='valid',
-            kernel_initializer=K.initializers.HeNormal(seed=None)
-        ),
-        K.layers.MaxPooling2D((2, 2), strides=(2, 2)),
-        K.layers.Flatten(),
-        K.layers.Dense(
-            120,
-            activation='relu',
-            kernel_initializer=K.initializers.HeNormal(seed=None)
-        ),
-        K.layers.Dense(
-            84,
-            activation='relu',
-            kernel_initializer=K.initializers.HeNormal(seed=None)
-        ),
-        K.layers.Dense(
-            10,
-            activation='softmax',
-            kernel_initializer=K.initializers.HeNormal(seed=None)
-        ),
-    ])
+    conv1 = K.layers.Conv2D(
+        6,
+        (5, 5),
+        activation='relu',
+        padding='same',
+        kernel_initializer=K.initializers.HeNormal(seed=None),
+        input_shape=(28, 28, 1)
+    )(X)
+    pool1 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv1)
+    conv2 = K.layers.Conv2D(
+        16,
+        (5, 5),
+        activation='relu',
+        padding='valid',
+        kernel_initializer=K.initializers.HeNormal(seed=None)
+    )(pool1)
+    pool2 = K.layers.MaxPooling2D((2, 2), strides=(2, 2))(conv2)
+    flatten = K.layers.Flatten()(pool2)
+    dense1 = K.layers.Dense(
+        120,
+        activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=None)
+    )(flatten)
+    dense2 = K.layers.Dense(
+        84,
+        activation='relu',
+        kernel_initializer=K.initializers.HeNormal(seed=None)
+    )(dense1)
+    dense3 = K.layers.Dense(
+        10,
+        activation='softmax',
+        kernel_initializer=K.initializers.HeNormal(seed=None)
+    )(dense2)
+    model = K.Model(inputs=X, outputs=dense3)
     model.compile(
         optimizer='adam',
         loss='categorical_crossentropy',
