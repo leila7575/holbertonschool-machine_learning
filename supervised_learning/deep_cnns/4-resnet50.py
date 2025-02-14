f7d9e8d587a9d88d0862bea2ea056fafc71e11ef
leila7575 <leila.louajri@gmail.com>
2025-02-14 23:23:41 +0100

    4-resnet50.py

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -29,23 +29,27 @@ def resnet50():
         (3, 3), strides=(2, 2), padding='same'
     )(activation)
 
-    conv2_1 = projection_block(max_pooling2d, filters=(64, 64, 256))
+    # Modifier les blocs de convolution pour qu'ils retournent la bonne taille
+    projection_block = __import__('3-projection_block').projection_block
+    identity_block = __import__('2-identity_block').identity_block
+
+    conv2_1 = projection_block(max_pooling2d, filters=(64, 64, 256), s=1)
     conv2_2 = identity_block(conv2_1, filters=(64, 64, 256))
     conv2_3 = identity_block(conv2_2, filters=(64, 64, 256))
 
-    conv3_1 = projection_block(conv2_3, filters=(128, 128, 512))
+    conv3_1 = projection_block(conv2_3, filters=(128, 128, 512), s=2)
     conv3_2 = identity_block(conv3_1, filters=(128, 128, 512))
     conv3_3 = identity_block(conv3_2, filters=(128, 128, 512))
     conv3_4 = identity_block(conv3_3, filters=(128, 128, 512))
 
-    conv4_1 = projection_block(conv3_4, filters=(256, 256, 1024))
+    conv4_1 = projection_block(conv3_4, filters=(256, 256, 1024), s=2)
     conv4_2 = identity_block(conv4_1, filters=(256, 256, 1024))
     conv4_3 = identity_block(conv4_2, filters=(256, 256, 1024))
     conv4_4 = identity_block(conv4_3, filters=(256, 256, 1024))
     conv4_5 = identity_block(conv4_4, filters=(256, 256, 1024))
     conv4_6 = identity_block(conv4_5, filters=(256, 256, 1024))
 
-    conv5_1 = projection_block(conv4_6, filters=(512, 512, 2048))
+    conv5_1 = projection_block(conv4_6, filters=(512, 512, 2048), s=2)
     conv5_2 = identity_block(conv5_1, filters=(512, 512, 2048))
     conv5_3 = identity_block(conv5_2, filters=(512, 512, 2048))
 
