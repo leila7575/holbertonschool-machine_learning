d4750dbf37642eba3bb319ce557593c5b8db8fc0
leila7575 <leila.louajri@gmail.com>
2025-02-14 05:02:11 +0100

    0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -10,28 +10,28 @@ def inception_block(A_prev, filters):
     F1, F3R, F3, F5R, F5, FPP = filters
 
     conv2d = K.layers.Conv2D(
-        F1, (1, 1), padding='same', activation='relu', name='conv2d'
+        F1, (1, 1), padding='same', activation='relu'
     )(A_prev)
 
     conv2d_1 = K.layers.Conv2D(
-        F3R, (1, 1), padding='same', activation='relu', name='conv2d_1'
+        F3R, (1, 1), padding='same', activation='relu'
     )(A_prev)
     conv2d_2 = K.layers.Conv2D(
-        F3, (3, 3), padding='same', activation='relu', name='conv2d_2'
+        F3, (3, 3), padding='same', activation='relu'
     )(conv2d_1)
 
     conv2d_3 = K.layers.Conv2D(
-        F5R, (1, 1), padding='same', activation='relu', name='conv2d_3'
+        F5R, (1, 1), padding='same', activation='relu'
     )(A_prev)
     conv2d_4 = K.layers.Conv2D(
-        F5, (5, 5), padding='same', activation='relu', name='conv2d_4'
+        F5, (5, 5), padding='same', activation='relu'
     )(conv2d_3)
 
     max_pooling2d = K.layers.MaxPooling2D(
-        (3, 3), strides=(1, 1), padding='same', name='max_pooling2d'
+        (3, 3), strides=(1, 1), padding='same'
     )(A_prev)
     conv2d_5 = K.layers.Conv2D(
-        FPP, (1, 1), padding='same', activation='relu', name='conv2d_5'
+        FPP, (1, 1), padding='same', activation='relu'
     )(max_pooling2d)
 
     concatenate = K.layers.Concatenate(name='concatenate')(
