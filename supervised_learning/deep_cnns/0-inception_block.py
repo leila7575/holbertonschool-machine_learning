c06278f78e6acced87fc8678ccc3e9e85296b34d
leila7575 <leila.louajri@gmail.com>
2025-02-14 05:05:27 +0100

    0-inception_block.py

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -34,7 +34,7 @@ def inception_block(A_prev, filters):
         FPP, (1, 1), padding='same', activation='relu'
     )(max_pooling2d)
 
-    concatenate = K.layers.Concatenate(name='concatenate')(
+    concatenate = K.layers.Concatenate(
         [conv2d, conv2d_2, conv2d_4, conv2d_5]
     )
 
