229f0b0b766e873c935a2ae4e61d5476fbb379cf
leila7575 <leila.louajri@gmail.com>
2025-03-30 19:48:21 +0200

    adding 1-pca file

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -7,8 +7,9 @@ import numpy as np
 
 def pca(X, ndim):
     """Performs PCA on a dataset with SVD"""
-    left_sing_vect, sing_values, right_sing_vect = np.linalg.svd(X)
-    W = right_sing_vect.T[:, :ndim]
-    T = np.matmul(X, W)
+    X_centered = X - np.mean(X, axis=0)
+    left_sing_vect, sing_values, right_sing_vect = np.linalg.svd(X_centered)
+    W = right_sing_vect[:ndim].T
+    T = np.matmul(X_centered, W)
 
     return T
