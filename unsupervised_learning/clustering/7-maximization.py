c489044c75a98b71e321f66ec0feeb0bb857d3c3
leila7575 <leila.louajri@gmail.com>
2025-04-06 23:46:34 +0200

    7-maximization.py

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -7,14 +7,15 @@ import numpy as np
 
 def maximization(X, g):
     """computes maximization step for GMM."""
-    n, d = X.shape
-    k = g.shape[0]
-    pi = np.sum(g, axis=1) / n
-    m = np.dot(g, X) / np.sum(g, axis=1)[:, None]
-
-    S = np.zeros((k, d, d))
-    for i in range(k):
-        X_centered = X - m[i]
-        S[i] = np.dot(g[i] * X_centered.T, X_centered) / np.sum(g[i])
-
-    return pi, m, S
+    try:
+        n, d = X.shape
+        k = g.shape[0]
+        pi = np.sum(g, axis=1) / n
+        m = np.dot(g, X) / np.sum(g, axis=1)[:, None]
+        S = np.zeros((k, d, d))
+        for i in range(k):
+            X_centered = X - m[i]
+            S[i] = np.dot(g[i] * X_centered.T, X_centered) / np.sum(g[i])
+            return pi, m, S
+    except Exception:
+        return None, None, None
