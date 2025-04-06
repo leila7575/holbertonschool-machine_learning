01f1636b571a4ac69d453bd28424018d43551a5e
leila7575 <leila.louajri@gmail.com>
2025-04-06 23:41:16 +0200

    fixing pycodestyle

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def pdf(X, m, S):
     """computes pdf for GMM."""
-    
+
     try:
         n, d = X.shape
         X_centered = X - m
@@ -15,8 +15,8 @@ def pdf(X, m, S):
         cov_mat_inverse = np.linalg.inv(S)
         norm_const = 1 / (((2 * np.pi) ** d) * det) ** 0.5
         exp_val = np.sum(
-			(np.matmul(X_centered, cov_mat_inverse)) * X_centered, axis=1
-		)
+            (np.matmul(X_centered, cov_mat_inverse)) * X_centered, axis=1
+        )
         pdf_value = norm_const * np.exp(-0.5 * exp_val)
         P = np.maximum(pdf_value, 1e-300)
         return P
