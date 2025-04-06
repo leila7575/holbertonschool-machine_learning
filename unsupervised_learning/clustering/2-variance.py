fe091f7ef902d085af4ec7a1bdad56eed6c656cf
leila7575 <leila.louajri@gmail.com>
2025-04-06 23:38:18 +0200

    2-variance.py

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -7,14 +7,15 @@ import numpy as np
 
 def variance(X, C):
     """Computes total intra-cluster variance for a dataste."""
-    n, d = X.shape
-    k, _ = C.shape
-
-    euclidean_dist = np.sum(
-        ((X[:, np.newaxis, :] - C[np.newaxis, :, :]) ** 2), axis=2
-    )
-    closest_centroid = np.argmin(euclidean_dist, axis=1)
-    centroids = C[closest_centroid]
-    var = np.sum(np.sum((X - centroids) ** 2, axis=1))
-
-    return var
+    try:
+        n, d = X.shape
+        k, _ = C.shape
+        euclidean_dist = np.sum(
+            ((X[:, np.newaxis, :] - C[np.newaxis, :, :]) ** 2), axis=2
+        )
+        closest_centroid = np.argmin(euclidean_dist, axis=1)
+        centroids = C[closest_centroid]
+        var = np.sum(np.sum((X - centroids) ** 2, axis=1))
+        return var
+    except Exception:
+        return None
