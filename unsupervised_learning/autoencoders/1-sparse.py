a16100864ac446742c3db1ffa555dc7f0012028b
leila7575 <leila.louajri@gmail.com>
2025-04-23 22:18:52 +0200

    adding sparse autoencoder function

---
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Contains autencoder function"""
+
+
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
+    """Creates a sparse autoencoder"""
+    input = keras.layers.Input(shape=(input_dims,))
+    encoder = keras.layers.Dense(hidden_layers[0], activation='relu')(input)
+    for i in hidden_layers[1:]:
+        encoder = keras.layers.Dense(i, activation='relu')(encoder)
+    encoder = keras.layers.Dense(
+        latent_dims, activation='relu',
+        activity_regularizer=keras.regularizers.L1(lambtha)
+    )(encoder)
+
+    decoder_input = keras.layers.Input(shape=latent_dims,)
+    decoder = keras.layers.Dense(
+        hidden_layers[-1], activation='relu'
+    )(decoder_input)
+    for i in (reversed(hidden_layers[:-1])):
+        decoder = keras.layers.Dense(i, activation='relu')(decoder)
+    decoder_output = keras.layers.Dense(
+        input_dims, activation='sigmoid'
+    )(decoder)
+
+    encoder = keras.models.Model(input, encoder)
+    decoder = keras.models.Model(decoder_input, decoder_output)
+    output = decoder(encoder.output)
+    auto = keras.models.Model(input, output)
+
+    auto.compile(optimizer='Adam', loss='binary_crossentropy')
+
+    return encoder, decoder, auto
