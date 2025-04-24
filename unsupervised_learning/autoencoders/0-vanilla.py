fd46957715ee2571bf92c7106beaedc53780f163
leila7575 <leila.louajri@gmail.com>
2025-04-24 23:21:04 +0200

    Update: changed encoder and decoder layers

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -8,22 +8,22 @@ import tensorflow.keras as keras
 def autoencoder(input_dims, hidden_layers, latent_dims):
     """Creates an autoencoder"""
     input = keras.layers.Input(shape=(input_dims,))
-    encoder = keras.layers.Dense(hidden_layers[0], activation='relu')(input)
+    encoder_layer = keras.layers.Dense(hidden_layers[0], activation='relu')(input)
     for i in hidden_layers[1:]:
-        encoder = keras.layers.Dense(i, activation='relu')(encoder)
-    encoder = keras.layers.Dense(latent_dims, activation='linear')(encoder)
+        encoder_layer = keras.layers.Dense(i, activation='relu')(encoder_layer)
+    encoder_layer = keras.layers.Dense(latent_dims, activation='relu')(encoder_layer)
 
     decoder_input = keras.layers.Input(shape=latent_dims,)
-    decoder = keras.layers.Dense(
+    decoder_layer = keras.layers.Dense(
         hidden_layers[-1], activation='relu'
     )(decoder_input)
     for i in (reversed(hidden_layers[:-1])):
-        decoder = keras.layers.Dense(i, activation='relu')(decoder)
+        decoder_layer = keras.layers.Dense(i, activation='relu')(decoder_layer)
     decoder_output = keras.layers.Dense(
         input_dims, activation='sigmoid'
-    )(decoder)
+    )(decoder_layer)
 
-    encoder = keras.models.Model(input, encoder)
+    encoder = keras.models.Model(input, encoder_layer)
     decoder = keras.models.Model(decoder_input, decoder_output)
     output = decoder(encoder.output)
     auto = keras.models.Model(input, output)
