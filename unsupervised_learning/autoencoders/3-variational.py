3fe5945c516c405f49d1e625c9a4d28a0c9e8ac4
leila7575 <leila.louajri@gmail.com>
2025-04-27 21:34:32 +0200

    adding 3-variational file

--- unsupervised_learning/autoencoders/3-variational.py
+++ unsupervised_learning/autoencoders/3-variational.py
@@ -36,6 +36,6 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     output = decoder(encoder(input)[2])
     auto = keras.models.Model(input, output)
 
-    auto.compile(optimizer=optimizer, loss='binary_crossentropy')
+    auto.compile(optimizer='Adam', loss='binary_crossentropy')
 
     return encoder, decoder, auto
