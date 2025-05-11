fdca36cb360af8f7f1ad669aec618874a0455d20
leila7575 <leila.louajri@gmail.com>
2025-05-11 19:49:26 +0200

    added output method to bidirectional cell

---
+++ supervised_learning/RNNs/7-bi_output.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Class BidirectionalCell"""
+
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """Bidirectional cell in RNN"""
+    def __init__(self, i, h, o):
+        """class constructor, initializes Bidirectional Cell attributes"""
+        self.i = i
+        self.h = h
+        self.o = o
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.bhf = np.zeros(shape=(1, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.bhb = np.zeros(shape=(1, h))
+        self.Wy = np.random.normal(size=(2 * h, o))
+        self.by = np.zeros(shape=(1, o))
+
+    def forward(self, h_prev, x_t):
+        """Performs forward propagation for one step"""
+        concatenated_h = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.dot(concatenated_h, self.Whf) + self.bhf)
+        return h_next
+
+    def backward(self, h_next, x_t):
+        """Computes hidden state in backward direction for one time step"""
+        concatenated_h = np.concatenate((h_next, x_t), axis=1)
+        h_pev = np.tanh(np.dot(concatenated_h, self.Whb) + self.bhb)
+        return h_pev
+    
+    def output(self, H):
+        """Calculates outputs"""
+        x = np.dot(H, self.Wy) + self.by
+        Y = self.softmax(x)
+        return Y
