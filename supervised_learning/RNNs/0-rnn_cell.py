3cf3ea43ffa527da159e03018b9d4e6be0a3af35
leila7575 <leila.louajri@gmail.com>
2025-05-11 07:09:21 +0200

    fixed shape mismatch on matrix multiplication

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -12,19 +12,20 @@ class RNNCell:
         self.i = i
         self.h = h
         self.o = o
-        self.Wh = np.random.normal(size=(h, h + i))
+        self.Wh = np.random.normal(size=(h + i, h))
         self.bh = np.zeros(shape=(h, 1))
         self.Wy = np.random.normal(size=(o, h))
         self.by = np.zeros(shape=(o, 1))
 
     def softmax(self, x):
+        """Applies softmax function"""
         exp_x = np.exp(x - np.max(x))
         return exp_x / np.sum(exp_x)
 
     def forward(self, h_prev, x_t):
         """Performs forward propagation for one step"""
         concatenated_h = np.concatenate((h_prev, x_t), axis=1)
-        h_next = np.tanh(np.dot(concatenated_h, self.Wh.T) + self.bh)
+        h_next = np.tanh(np.dot(concatenated_h, self.Wh) + self.bh)
         x = np.dot(h_next, self.Wy.T) + self.by
         y = self.softmax(x)
         return h_next, y
