b9711888f5c88a4d72d5d8ef7287ed1aa179d120
leila7575 <leila.louajri@gmail.com>
2025-07-13 18:55:24 +0200

    1-td_lambtha.py

---
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""First-visit Monte Carlo algorithm"""
+
+
+import gymnasium as gym
+
+
+def monte_carlo(
+    env, V, policy, episodes=5000,
+    max_steps=100, alpha=0.1, gamma=0.99
+):
+    """Estimates V-value with first-visit Monte Carlo algorithm"""
+    for episode in range(episodes):
+        state, _ = env.reset()
+        episode_info = []
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, terminated, truncated, info = env.step(action)
+            episode_info.append((state, action, reward))
+            if terminated or truncated:
+                break
+            state = next_state
+
+        G = 0
+        for ep_index in range(len(episode_info) - 1, - 1, - 1):
+            state, action, reward = episode_info[ep_index]
+            G = gamma * G + reward
+            visited_states = [
+                prev_state for prev_state, _, _ in episode_info[:episode]
+            ]
+            if state not in visited_states:
+                V[state] = V[state] + alpha * (G - V[state])
+
+    return V
---
+++ reinforcement_learning/temporal_difference/1-td_lambtha.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Temporal difference algorithm with trace decay parameter"""
+
+
+import gymnasium as gym
+import numpy as np
+
+
+def td_lambtha(
+    env, V, policy, lambtha, episodes=5000,
+    max_steps=100, alpha=0.1, gamma=0.99
+):
+    """Updates V value based on TD with trace decay algorithm"""
+    for episode in range(episodes):
+        E = np.zeros(env.observation_space.n)
+        state, _ = env.reset()
+        for step in range(max_steps):
+            action = policy(state)
+            next_state, reward, terminated, truncated, info = env.step(action)
+            delta = reward + gamma * V[next_state] - V[state]
+            E[state] += 1
+            V += alpha * delta * E
+            E *= gamma * lambtha
+            state = next_state
+            if terminated or truncated:
+                break
+    return V
