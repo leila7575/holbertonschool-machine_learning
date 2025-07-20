fb7a3ebf4d1612d7fcf4887c0a67123d27ff0bf4
leila7575 <leila.louajri@gmail.com>
2025-07-20 23:50:50 +0200

    train.py

--- reinforcement_learning/policy_gradients/train.py
+++ reinforcement_learning/policy_gradients/train.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 policy_gradient = __import__('policy_gradient').policy_gradient
 
 
-def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98, show_result=False):
     """Training loop implementation with monte carlo policy gradient"""
     scores = []
     states = env.observation_space.shape[0]
@@ -20,6 +20,8 @@ def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
         episode_info = []
 
         while True:
+            if show_result and episode % 1000 == 0:
+                env.render()
             action, gradient = policy_gradient(state, weights)
             next_state, reward, terminated, truncated, info = env.step(action)
             episode_info.append((state, action, reward, gradient))
