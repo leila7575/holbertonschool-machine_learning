42d2150abdbeebd77f4d1ffc7db8a6f184ff3e3c
leila7575 <leila.louajri@gmail.com>
2025-07-23 11:21:21 +0200

    fixed pycodestyle

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,7 +2,7 @@
 """array selects values on DataFrame and converts it into numpy.ndarray"""
 
 
-def array (df):
+def array(df):
     """selects values on DataFrame and converts into numpy.ndarray"""
     df = df.loc[:, ['High', 'Close']].tail(10)
     np_ndarray = df.to_numpy()
