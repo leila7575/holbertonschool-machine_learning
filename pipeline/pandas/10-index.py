9c5474dc4361f6d07769875806092b009b2fa729
leila7575 <leila.louajri@gmail.com>
2025-07-24 00:55:27 +0200

    fixed pycodestyle

--- pipeline/pandas/10-index.py
+++ pipeline/pandas/10-index.py
@@ -2,7 +2,7 @@
 """index function reindexes the dataframe with timestamp column"""
 
 
-def index (df):
+def index(df):
     """Reindexing the dataframe with timestamp column"""
     df = df.set_index('Timestamp', drop='True')
     return df
