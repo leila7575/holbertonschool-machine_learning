d3ebc53c241927d8997c47a4adfbe43fe7efb045
leila7575 <leila.louajri@gmail.com>
2025-07-24 00:52:33 +0200

    adding rindexing function

---
+++ pipeline/pandas/10-index.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""index function reindexes the dataframe with timestamp column"""
+
+
+def index (df):
+    """Reindexing the dataframe with timestamp column"""
+    df = df.set_index('Timestamp', drop='True')
+    return df
