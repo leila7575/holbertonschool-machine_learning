96cbb4486744593a6f6dbb2e21d22bffdd5743bd
leila7575 <leila.louajri@gmail.com>
2025-07-28 18:31:14 +0200

    added multiindexs hierarchical indexing file

---
+++ pipeline/pandas/12-hierarchy.py
@@ -0,0 +1,25 @@
+#!/urs/bin/env python3
+"""Concatenates two sliced dataframe objects,
+rearranges indexes and sorts data."""
+
+
+import pandas as pd
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """Concatenates two sliced dataframe objects,
+    rearranges indexes and sorts data."""
+    df1 = index(df1)
+    df2 = index(df2)
+    concat_df = pd.concat(
+        [
+            df2.loc['1417411980':'1417417980', :],
+            df1.loc['1417411980':'1417417980', :]
+        ],
+        keys=['bitstamp', 'coinbase'],
+        axis=0
+    )
+    concat_df = concat_df.reorder_levels([1, 0])
+    concat_df = concat_df.sort_values(['Timestamp'], ascending=True)
+    return concat_df
