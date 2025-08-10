63cab241bd233ed2384d32cb0696323cf21d9707
leila7575 <leila.louajri@gmail.com>
2025-08-10 20:24:02 +0200

    2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -7,19 +7,27 @@ import requests
 import time
 
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
+def user_location(url):
+    """Prints user locations"""
+    try:
+        response = requests.get(url)
 
-    if response.status_code == 403:
-        reset = int(response.headers.get('X-Ratelimit-Reset', 0))
-        now = int(time.time())
-        X = (reset - now) / 60
-        print(f"Reset in {X} min")
+        if response.status_code == 200:
+            data = response.json()
+            print(data.get('location'))
+        elif response.status_code == 403:
+            reset = int(response.headers.get('X-Ratelimit-Reset', 0))
+            now = int(time.time())
+            X = (reset - now) / 60
+            print(f"Reset in {X} min")
+        else:
+            print("Not found")
 
-    if response.status_code == 200:
-        data = response.json()
-        print(data.get('location'))
-    else:
+    except requests.exceptions.RequestException:
         print("Not found")
-        sys.exit(1)
+
+
+if __name__ == '__main__':
+
+    url = sys.argv[1]
+    user_location(url)
