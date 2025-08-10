4c8bc731ff65658eb7eb73556911d175c9960a32
leila7575 <leila.louajri@gmail.com>
2025-08-10 22:56:08 +0200

    updated  28-update 32-update_topics

--- pipeline/databases/34-log_stats.py
+++ pipeline/databases/34-log_stats.py
@@ -20,3 +20,7 @@ def main():
 
     status_check = collection.count_documents({"method": "GET", "path": "/status"})
     print(f"{status_check} status check")
+    
+
+if __name__ == "__main__":
+    main()
