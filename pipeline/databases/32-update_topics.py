7d59ea005b78af17e986bafbdd336b1620dfc385
leila7575 <leila.louajri@gmail.com>
2025-08-10 22:51:39 +0200

    updated  28-update 32-update_topics

--- pipeline/databases/32-update_topics.py
+++ pipeline/databases/32-update_topics.py
@@ -10,4 +10,4 @@ def update_topics(mongo_collection, name: str, topics: List[str]):
     """Changes all topics of a school document based on the name"""
     query = {"name": name}
     update = {"$set": {"topics": topics}}
-    mongo_collection.update_one(query, update)
+    mongo_collection.update_many(query, update)
