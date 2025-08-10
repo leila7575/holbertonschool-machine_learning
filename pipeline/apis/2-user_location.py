63a5dbfae8a0c738bb2e5f3dd923f068bf1ff90d
leila7575 <leila.louajri@gmail.com>
2025-08-10 19:45:19 +0200

    2-user_location.py

--- pipeline/apis/2-user_location.py
+++ pipeline/apis/2-user_location.py
@@ -9,16 +9,16 @@ import time
 
 if __name__ == '__main__':
     url = sys.argv[1]
-    r = requests.get(url)
+    response = requests.get(url)
 
-    if r.status_code == 403:
-        reset = int(r.headers.get('X-Ratelimit-Reset', 0))
+    if response.status_code == 403:
+        reset = int(response.headers.get('X-Ratelimit-Reset', 0))
         now = int(time.time())
         X = (reset - now) / 60
         print(f"Reset in {X} min")
 
-    if r.status_code == 200:
-        data = r.json()
+    if response.status_code == 200:
+        data = response.json()
         print(data.get('location'))
     else:
         print("Not found")
