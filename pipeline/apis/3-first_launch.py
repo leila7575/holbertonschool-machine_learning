5c637bc970a11f4e6353cf757729026a37560111
leila7575 <leila.louajri@gmail.com>
2025-08-10 21:04:10 +0200

    3-first_launch.py

---
+++ pipeline/apis/3-first_launch.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import requests
+
+if __name__ == '__main__':
+    url = "https://api.spacexdata.com/v3/launches"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        launch_data = response.json()
+        launch_data.sort(key=lambda x: x.get('launch_date_unix', float('inf')))
+        launch = launch_data[0]
+        rocket_name = launch['rocket']['rocket_name']
+        launchpad_name = launch['launch_site']['site_name']
+        launchpad_locality = launch['launch_site']['site_name_long']
+        print(
+            f"{launch['mission_name']} ({launch['launch_date_local']}) "
+            f"{rocket_name} - {launchpad_name} ({launchpad_locality})"
+        )
+    else:
+        print('Not found')
